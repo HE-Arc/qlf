603dc99c81beb513771a4d07807d13a1ec42d791
ourfbht <ourfbht@gmail.com>
2019-10-24 15:25:42 +0200

    added seeding for users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Database\Seeder;
+use Illuminate\Database\Seeder;;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
