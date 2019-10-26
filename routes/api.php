e13db4becbdb5db27c4eb45c8a9a9a17ba001a19
ourfbht <ourfbht@gmail.com>
2019-10-26 18:17:07 +0200

    Revert "added user as a ressource. GET possible"
    This reverts commit 4f19663544d07c5a5c5ca2fd1ecb90834c41ec2b.

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Http\Request;
 
-use App\Http\Resources\User as UserResource;
-use App\User;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,6 +13,6 @@ use App\User;
 |
 */
 
-Route::get('/user', function () {
-    return new UserResource(User::find(1));
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
