4d4d747726415e698d5fc1611c81582c560013d2
qtipee <quent.vallat@gmail.com>
2019-11-14 09:28:21 +0100

    Removed duplicated ids in login.blade.php

--- resources/views/partials/login.blade.php
+++ resources/views/partials/login.blade.php
@@ -26,23 +26,23 @@
         <div class="row">
             <div class="input-field col s12">
                 <i class="material-icons prefix">account_box</i>
-                <input id="login-email" type="text" class="validate">
-                <label for="login-email">Email</label>
+                <input id="sign-email" type="text" class="validate">
+                <label for="sign-email">Email</label>
             </div>
             <div class="input-field col s12">
                 <i class="material-icons prefix">account_box</i>
-                <input id="login-username" type="text" class="validate">
-                <label for="login-username">Username</label>
+                <input id="sign-username" type="text" class="validate">
+                <label for="sign-username">Username</label>
             </div>
             <div class="input-field col s12">
                 <i class="material-icons prefix">vpn_key</i>
-                <input id="login-pw" type="password" class="validate">
-                <label for="login-pw">Password</label>
+                <input id="sign-pw" type="password" class="validate">
+                <label for="sign-pw">Password</label>
             </div>
             <div class="input-field col s12">
                 <i class="material-icons prefix">vpn_key</i>
-                <input id="login-pw-confirm" type="password" class="validate">
-                <label for="login-pw-confirm">Confirm password</label>
+                <input id="sign-pw-confirm" type="password" class="validate">
+                <label for="sign-pw-confirm">Confirm password</label>
             </div>
             <button class="btn pink waves-effect" type="submit" name="action">SIGN IN
                 <i class="material-icons right">send</i>
