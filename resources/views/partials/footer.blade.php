18afe99660a604aa58409d2098f5802b88e05d18
ourfbht <ourfbht@gmail.com>
2019-12-05 08:40:19 +0100

    / register will show on correct tab now #34

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,8 +7,8 @@
             <li class="tab"><a href="#tab-settings" class="white-text">Settings</a></li>
         @endauth
         @guest
-            <li class="tab"><a href="#tab-login" class="white-text" activate>Login</a></li>
-            <li class="tab"><a href="#tab-register" class="white-text">Register</a></li>
+            <li class="tab"><a href="#tab-login" class="white-text {{ request()->route()->getName() != 'register' ? 'active' : '' }}">Login</a></li>
+            <li class="tab"><a href="#tab-register" class="white-text {{ request()->route()->getName() == 'register' ? 'active' : '' }}">Register</a></li>
         @endguest
     </ul>
 </footer>
