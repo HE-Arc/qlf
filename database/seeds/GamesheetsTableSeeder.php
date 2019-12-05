e5ef6edfae5799377082f23860c2a900b69fab78
ourfbht <ourfbht@gmail.com>
2019-12-05 23:23:21 +0100

    displaying live game table from api (#21). Check todos in app.js.

--- database/seeds/GamesheetsTableSeeder.php
+++ database/seeds/GamesheetsTableSeeder.php
@@ -16,23 +16,9 @@ class GamesheetsTableSeeder extends Seeder
             'name' => 'Uno',
             'template' => '{
                 "type": "game",
-                "id": "0",
+                "id": "1",
+                "name": "Uno",
                 "attributes": {
-                  "name": "Uno",
-                  "players": {
-                    "Quentin": {
-                      "id": 0,
-                      "isPlaying": true
-                    },
-                    "Loic": {
-                      "id": 1,
-                      "isPlaying": false
-                    },
-                    "François": {
-                      "id": 2,
-                      "isPlaying": true
-                    }
-                  },
                   "column_header": {
                     "0": {
                       "player_id": 0
@@ -64,7 +50,8 @@ class GamesheetsTableSeeder extends Seeder
                   "content": {
                     "0": {
                       "0": 3,
-                      "1": 2
+                      "1": 2,
+                      "2": 0
                     },
                     "1": {
                       "0": 5,
@@ -73,9 +60,9 @@ class GamesheetsTableSeeder extends Seeder
                     },
                     "2": {
                       "0": 2,
+                      "1": 10,
                       "2": 10
-                    },
-                    "type": "col"
+                    }
                   },
                   "created": "2015-05-22T14:56:29.000Z",
                   "updated": "2015-05-22T14:56:28.000Z"
@@ -90,72 +77,59 @@ class GamesheetsTableSeeder extends Seeder
         DB::table('gamesheets')->insert([
             'name' => 'Yahtzee',
             'template' => '{
-                "type": "game",
-                "id": "0",
-                "attributes": {
-                    "name": "Uno",
-                    "players": {
-                        "Quentin": {
-                            "id": 0,
-                            "isPlaying": true
-                        },
-                        "Loic": {
-                            "id": 1,
-                            "isPlaying": false
-                        },
-                        "François": {
-                            "id": 2,
-                            "isPlaying": true
-                        }
-                    },
-                    "column_header": {
+                    "type": "game",
+                    "id": "2",
+                    "name": "Yahtzee",
+                    "attributes": {
+                      "column_header": {
                         "0": {
-                            "player_id": 0
+                          "player_id": 0
                         },
                         "1": {
-                            "player_id": 1
+                          "player_id": 1
                         },
                         "2": {
-                            "player_id": 2
+                          "player_id": 2
                         }
-                    },
-                    "row_header": {
+                      },
+                      "row_header": {
                         "0": {
-                            "id": 0,
-                            "text": "Round 1",
-                            "editable": false
+                          "id": 0,
+                          "text": "Round 1 y",
+                          "editable": false
                         },
                         "1": {
-                            "id": 1,
-                            "text": "Round 2",
-                            "editable": false
+                          "id": 1,
+                          "text": "Round 2 y",
+                          "editable": false
                         },
                         "2": {
-                            "id": 2,
-                            "text": "Final Round",
-                            "editable": true
+                          "id": 2,
+                          "text": "Final Round y",
+                          "editable": true
                         }
-                    },
-                    "content": {
-                        "type": "col",
+                      },
+                      "content": {
                         "0": {
-                            "0": 3,
-                            "1": 2
+                          "0": 33,
+                          "1": 22,
+                          "2": 02
                         },
                         "1": {
-                            "0": 5,
-                            "1": 2,
-                            "2": 3
+                          "0": 53,
+                          "1": 22,
+                          "2": 33
                         },
                         "2": {
-                            "0": 2,
-                            "2": 10
+                          "0": 22,
+                          "1": 101,
+                          "2": 102
                         }
-                    },
-                    "created": "2015-05-22T14:56:29.000Z",
-                    "updated": "2015-05-22T14:56:28.000Z"
-                }
-            }',
+                      },
+                      "created": "2015-05-22T14:56:29.000Z",
+                      "updated": "2015-05-22T14:56:28.000Z"
+                    }
+                  }',
             'downloads' => 56,
             'user_id' => 1,
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
--- public/js/app.js
+++ public/js/app.js
@@ -49,81 +49,6 @@ document.addEventListener('DOMContentLoaded', () =>
     }
 });
 
-var json = `
-{
-    "data": {
-        "type": "game",
-        "id": "0",
-        "attributes": {
-            "name": "Uno",
-            "players": {
-                "Quentin": {
-                    "id": 0,
-                    "isPlaying": true
-                },
-                "Loic": {
-                    "id": 1,
-                    "isPlaying": false
-                },
-                "François": {
-                    "id": 2,
-                    "isPlaying": true
-                }
-            },
-            "column_header": {
-                "0": {
-                    "player_id": 0,
-                    "text": "Player 0"
-                },
-                "1": {
-                    "player_id": 1,
-                    "text": "Player 1"
-                },
-                "2": {
-                    "player_id": 2,
-                    "text": "Player 2"
-                }
-            },
-            "row_header": {
-                "0": {
-                    "id": 0,
-                    "text": "Round 1",
-                    "editable": false
-                },
-                "1": {
-                    "id": 1,
-                    "text": "Round 2",
-                    "editable": false
-                },
-                "2": {
-                    "id": 2,
-                    "text": "Final Round",
-                    "editable": true
-                }
-            },
-            "content": {
-                "type": "col",
-                "0": {
-                    "0": 3,
-                    "1": 2
-                },
-                "1": {
-                    "0": 5,
-                    "1": 2,
-                    "2": 3
-                },
-                "2": {
-                    "0": 2,
-                    "2": 10
-                }
-            },
-            "created": "2015-05-22T14:56:29.000Z",
-            "updated": "2015-05-22T14:56:28.000Z"
-        }
-    }
-}
-`;
-
 function parseJsonGame(json)
 {
     let game = JSON.parse(json);
@@ -193,16 +118,18 @@ function parseJsonGameTemplate(json)
     let table = '<table><thead><tr><th></th>';
     for (let col in columns)
     {
-        table += '<th>' + columns[col].text + '</th>';
+        // TODO: player's id shouldnt be fetched from "gamesheets" but from "games". To change.
+        table += '<th> player\'s ID is: ' + columns[col].player_id + '</th>';
     }
     table += '</tr></thead>';
 
     for (let row in rows)
     {
         table += '<tr><th>' + rows[row].text + '</th>';
-        for (let tmp in columns)
+        for (let col in columns)
         {
-            table += '<td>Empty</td>';
+            // TODO: players' scores shouldn't be fetched from "gamesheets" but from "games". To change.
+            table += '<td>' + content[row][col] + '</td>';
         }
         table += '</tr>';
     }
