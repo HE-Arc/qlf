874db625bb303d7c4e759e07fc676674a982a8a3
ourfbht <ourfbht@gmail.com>
2019-12-16 22:14:31 +0100

    commented fct parseJsonMarketTemplate

--- public/js/app.js
+++ public/js/app.js
@@ -73,6 +73,9 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
 
+/**
+ * Display all gamesheet in market.
+ */
 function parseJsonMarketTemplate(json){
     gamesheets = json.data;
 
